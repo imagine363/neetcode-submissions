71e37430b91dd19facc19ac6cf42030efbfc4ad1
LeMinh <47525918+imagine363@users.noreply.github.com>
2026-09-02 15:47:02 +0700

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def letterCombinations(self, digits):
+        phone = {"2": "abc","3": "def","4": "ghi","5": "jkl","6": "mno","7": "pqrs","8": "tuv","9": "wxyz"}
+
+        res = []
+        comb = []
+        def backtrack(start):
+            if len(comb) == len(digits):
+                res.append("".join(comb))
+                return
+            button = phone[digits[start]]
+            for i in range(len(button)):
+                comb.append(button[i])
+                backtrack(start+1)
+                comb.pop()
+        if not digits:
+            return []
+        backtrack(0)
+        return res
+
+sol = Solution()
+digits = "34"
+print(sol.letterCombinations(digits))
+
+        
